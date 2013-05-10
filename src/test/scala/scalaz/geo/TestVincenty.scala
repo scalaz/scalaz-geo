59f86d9591eea58e0a52130ed97aa8cf8f58804a
folone <folone@gmail.com>
2013-05-10 13:17:44 +0200

    Fixed the tests.

--- src/test/scala/scalaz/geo/TestVincenty.scala
+++ src/test/scala/scalaz/geo/TestVincenty.scala
@@ -1,16 +1,13 @@
 package scalaz.contrib.geo
 
-import org.specs2.scalaz.Spec
+import org.specs2.scalaz.ScalazMatchers
+import org.specs2.Specification
 
-import scalaz.scalacheck.ScalazArbitrary._
-import scalaz.scalacheck.ScalazProperties._
-
-class TestVincenty extends Spec {
-  import scalaz.Scalaz._
+class TestVincenty extends Specification with ScalazMatchers {
   import scalaz.contrib.geo._
   import Geo._
 
-  "Vincenty test" >> {
+  def is = "Vincenty test" ! {
     // Mount Barney, Queensland, Australia
     val mountBarney = -28.2807 |-| 152.698
 
@@ -18,9 +15,8 @@ class TestVincenty extends Spec {
     val ngaPeak = -22.6528 |-| 167.4619
 
     // Travelling at 11.5 degrees for a little over 150km from Mount Barney places you at the given vector.
-    mountBarney.direct(bearing(11.5D), 150435D) must be_===(vector(-26.950066610300084D |-| 153.0000106566432D, bearing(11.359998078380356D)))
-
+    (mountBarney.direct(bearing(11.5D), 150435D) must equal(vector(-26.950066610300084D |-| 153.0000106566432D, bearing(11.359998078380356D)))) and
     // Mount Barney to N'ga Peak is 1608.695km
-    (mountBarney inverse ngaPeak) must be_===(curve(1608695.5945547633, azimuth(0D), azimuth(180D)))
+    ((mountBarney inverse ngaPeak) must equal(curve(1608695.5945547633, azimuth(0D), azimuth(180D))))
   }
 }
